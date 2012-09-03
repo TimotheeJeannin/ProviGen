4e2297ecdf7ee4f84f0fa25861362f0a4f060412
TimotheeJeannin <timojeajea@gmail.com>
2012-09-03 13:03:08 +0200

    Moved annotaion in their own package.

--- src/com/tjeannin/provigen/annotations/Column.java
+++ src/com/tjeannin/provigen/annotations/Column.java
@@ -1,9 +1,9 @@
-package com.tjeannin.provigen;
+package com.tjeannin.provigen.annotations;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Table {
+public @interface Column {
 	String authority();
 }
--- src/com/tjeannin/provigen/annotations/Table.java
+++ src/com/tjeannin/provigen/annotations/Table.java
@@ -1,9 +1,9 @@
-package com.tjeannin.provigen;
+package com.tjeannin.provigen.annotations;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Column {
+public @interface Table {
 	String authority();
 }
