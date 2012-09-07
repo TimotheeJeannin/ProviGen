2ae8246d15aa9369e8073756404445db6c3e9709
Timothée Jeannin <timojeajea@gmail.com>
2012-09-07 22:46:12 +0200

    Avanced sample project.

--- ProviGenSample/src/com/tjeannin/provigen/sample/MainActivity.java
+++ ProviGenSample/src/com/tjeannin/provigen/sample/MainActivity.java
@@ -1,22 +1,49 @@
 package com.tjeannin.provigen.sample;
 
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.view.Menu;
+import android.widget.ListView;
+
+import com.tjeannin.provigen.sample.SampleContentProvider.SampleContract;
 import com.tjeannin.provigensample.R;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+public class MainActivity extends FragmentActivity implements LoaderCallbacks<Cursor> {
 
-public class MainActivity extends Activity {
+	private ListView listView;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+		listView = (ListView) findViewById(R.id.list_view);
+
+		getSupportLoaderManager().initLoader(0, null, this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		return new CursorLoader(this, SampleContract.CONTENT_URI, null, "", null, "");
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+	}
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		// TODO Auto-generated method stub
+
+	}
 }
