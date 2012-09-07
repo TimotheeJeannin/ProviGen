d894c0255a93ff4440294142a9b21e35c6842ed6
Timothée Jeannin <timojeajea@gmail.com>
2012-09-07 04:59:46 -0700

    fqsdfds

--- src/com/tjeannin/provigen/Type.java
+++
@@ -1,9 +0,0 @@
-package com.tjeannin.provigen;
-
-public class Type {
-
-	public static final String INTEGER = "INTEGER";
-	public static final String REAL = "REAL";
-	public static final String TEXT = "TEXT";
-	public static final String BLOB = "BLOB";
-}
