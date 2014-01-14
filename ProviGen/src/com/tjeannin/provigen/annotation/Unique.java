640a9e6348ef37e2cfcfe20293bac4ac7e1d758b
Timothée Jeannin <timojeajea@gmail.com>
2014-01-14 20:50:35 +0100

    Fixed constraint annotations javadoc.

--- ProviGen/src/com/tjeannin/provigen/annotation/Unique.java
+++ ProviGen/src/com/tjeannin/provigen/annotation/Unique.java
@@ -9,11 +9,11 @@ import java.lang.annotation.Target;
  * Identifies a field of a contract class that should be unique in the database.</br>
  * The value attribute should be one of the following:
  * <ul>
- * <li>{@link OnConflict#ROLLBACK}</li>
- * <li>{@link OnConflict#ABORT}</li>
- * <li>{@link OnConflict#FAIL}</li>
- * <li>{@link OnConflict#IGNORE}</li>
- * <li>{@link OnConflict#REPLACE}</li>
+ * <li>{@link com.tjeannin.provigen.Constraint.OnConflict#ROLLBACK}</li>
+ * <li>{@link com.tjeannin.provigen.Constraint.OnConflict#ABORT}</li>
+ * <li>{@link com.tjeannin.provigen.Constraint.OnConflict#FAIL}</li>
+ * <li>{@link com.tjeannin.provigen.Constraint.OnConflict#IGNORE}</li>
+ * <li>{@link com.tjeannin.provigen.Constraint.OnConflict#REPLACE}</li>
  * </ul>
  * This constraint is <b>only</b> applied on table creation. <br/>
  * Adding this annotation to a {@link Contract} with an already created table will have <b>no effect</b>.
