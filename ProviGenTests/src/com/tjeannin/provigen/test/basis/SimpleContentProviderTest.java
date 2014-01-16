f0d386a910df59f4dacb2c06daf786185f465846
Timothée Jeannin <timojeajea@gmail.com>
2014-01-16 19:59:47 +0100

    Added more tests for basic behaviors. (bulkInsert, update multiple, delete multiple)

--- ProviGenTests/src/com/tjeannin/provigen/test/basis/SimpleContentProviderTest.java
+++ ProviGenTests/src/com/tjeannin/provigen/test/basis/SimpleContentProviderTest.java
@@ -54,6 +54,45 @@ public class SimpleContentProviderTest extends ExtendedProviderTestCase<SimpleCo
 		assertEquals(0, getRowCount(ContractOne.CONTENT_URI));
 	}
 
+	public void testBulkInsert() {
+
+		ContentValues[] contentValuesArray = new ContentValues[10];
+		for (int i = 0; i < 10; i++) {
+			contentValuesArray[i] = getContentValues(ContractOne.class);
+		}
+
+		contentResolver.bulkInsert(ContractOne.CONTENT_URI, contentValuesArray);
+		assertEquals(10, getRowCount(ContractOne.CONTENT_URI));
+	}
+
+	public void testUpdateMultiple() {
+		ContentValues contentValues = new ContentValues();
+		contentValues.put(ContractOne.MY_INT, 5);
+		contentResolver.insert(ContractOne.CONTENT_URI, contentValues);
+		contentValues.put(ContractOne.MY_INT, 7);
+		contentResolver.insert(ContractOne.CONTENT_URI, contentValues);
+
+		contentValues.put(ContractOne.MY_INT, 9);
+		contentResolver.update(ContractOne.CONTENT_URI, contentValues, "", null);
+
+		Cursor cursor = contentResolver.query(ContractOne.CONTENT_URI, null, "", null, "");
+		assertEquals(2, getRowCount(ContractOne.CONTENT_URI));
+		while (cursor.moveToNext()) {
+			assertEquals(9, cursor.getInt(cursor.getColumnIndex(ContractOne.MY_INT)));
+		}
+	}
+
+	public void testDeleteMultiple() {
+
+		contentResolver.insert(ContractOne.CONTENT_URI, getContentValues(ContractOne.class));
+		contentResolver.insert(ContractOne.CONTENT_URI, getContentValues(ContractOne.class));
+		contentResolver.insert(ContractOne.CONTENT_URI, getContentValues(ContractOne.class));
+
+		assertEquals(3, getRowCount(ContractOne.CONTENT_URI));
+		contentResolver.delete(ContractOne.CONTENT_URI, "", null);
+		assertEquals(0, getRowCount(ContractOne.CONTENT_URI));
+	}
+
 	public void testAutoIncrement() {
 
 		contentResolver.insert(ContractOne.CONTENT_URI, getContentValues(ContractOne.class));
